1399761d477aa4478c7a4f1623122ad4ac785316
Lucas Josino <contact@lucasjosino.com>
2024-11-20 09:13:16 -0300

    feat: adiciona Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup></script>
+
+<template>
+  <footer>
+    <div class="footer-container">
+      <Logo />
+    </div>
+  </footer>
+</template>
+
+<style>
+footer {
+  height: 10rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer-container {
+  max-width: var(--max-width);
+}
+</style>
