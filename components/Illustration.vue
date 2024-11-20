d65f2dea1ebc40ed3fafe54ef27868180c6c10a1
Lucas Josino <contact@lucasjosino.com>
2024-11-20 09:16:19 -0300

    feat: adiciona componente `Illustration` e modulo `@nuxt/image`

---
+++ components/Illustration.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps({ src: { type: String, required: true }, alt: { type: String, required: true } })
+</script>
+
+<template>
+  <div class="illustration-container">
+    <NuxtImg :src="src" alt="alt" class="illustration" />
+  </div>
+</template>
+
+<style>
+.illustration-container {
+  height: 100%;
+  width: 60%;
+  pointer-events: none;
+}
+
+.illustration {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+}
+</style>
