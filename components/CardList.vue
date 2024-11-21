cf3a66fc8b935e4471662d83a31cc6d86333888b
Lucas Josino <contact@lucasjosino.com>
2024-11-21 16:15:35 -0300

    feat: adiciona componente `CardList`

---
+++ components/CardList.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="card-list">
+    <slot />
+  </div>
+</template>
+
+<style>
+.card-list {
+  width: 100%;
+  max-width: var(--max-width);
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(410px, 1fr));
+  gap: 1rem;
+  justify-items: center;
+  align-items: center;
+}
+</style>
