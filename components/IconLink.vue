083d33c3502b2ddc7df83eb0e08e5480687fe2ce
Lucas Josino <contact@lucasjosino.com>
2024-11-20 21:29:48 -0300

    feat: adiciona componente `IconLink`

---
+++ components/IconLink.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const props = defineProps({ icon: { type: String, required: true }, href: { type: String, required: true }, title: { type: String, required: false } })
+</script>
+
+<template>
+  <NuxtLink :href="href" :title="title" class="icon-link">
+    <Icon :name="icon" class="icon-link-icon" />
+  </NuxtLink>
+</template>
+
+<style>
+.icon-link {
+  background-color: transparent;
+  color: var(--color-text-accent);
+  padding: var(--spacing-sm) var(--spacing-xl);
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-small);
+  transition: background-color var(--transition-normal);
+  align-content: center;
+  border: 1px solid var(--color-primary);
+}
+
+.icon-link-icon {
+  color: var(--color-primary);
+  transition: color var(--transition-normal);
+}
+
+.icon-link:hover {
+  background-color: var(--color-primary);
+}
+
+.icon-link:hover>.icon-link-icon {
+  color: white;
+}
+</style>
