0afdded1bb314e32aaf935abe8c62648e0e2e4e1
Lucas Josino <contact@lucasjosino.com>
2024-11-20 21:30:29 -0300

    refactor: atualiza estrutura do componente `ImageCard`

--- components/ImageCard.vue
+++ components/ImageCard.vue
@@ -1,56 +1,49 @@
-<template>
-    <div class="card_image">
-        <img src="../public/images/Dog.png" alt="" class="dog">
+<script lang="ts" setup></script>
 
-        <div class="imformacoes">
-            <div class="inf_img">
-                <img src="../public/images/Vector.png" alt="" class="vetor">
-                <p>81096086-27ab-492c-bb80-db800b...</p>
+<template>
+    <div class="image-card">
+        <img src="https://picsum.photos/1200/630" alt="" class="image-card-image">
+
+        <div class="image-card-content">
+            <div class="image-card-info">
+                <Icon name="mdi:image" />
+                <span>81096086-27ab-492c-bb80-db800b...</span>
             </div>
 
-            <div class="inf_time">
-                <img src="../public/images/AccessTime.png" alt="" class="vetor">
-                <p>12 de nov. de 2024</p>
+            <div class="image-card-info">
+                <Icon name="mdi:clock-outline" />
+                <span>12 de nov. de 2024</span>
             </div>
         </div>
-
     </div>
 </template>
 
-<script>
-    export default {
-        
-    }
-</script>
-
 <style scoped>
-.imformacoes{
+.image-card {
+    border: 1px solid var(--color-border);
+    width: 425px;
+    border-radius: var(--border-radius-md);
+    padding: var(--card-min-padding) var(--card-min-padding);
+    box-shadow: var(--shadow-small);
+}
+
+.image-card-image {
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius-md);
+}
+
+.image-card-content {
     display: flex;
     flex-direction: column;
-    margin-top:10px;
+    margin-top: 10px;
     gap: 10px;
-    }
-    .dog {
-        width: 100%;
+}
 
-    }
-   .card_image {
-    border: solid 2px rgba(0, 0, 0, 0.281);
-    border-radius: 25px;
-    width: 340px;
-    height: 370px;
-    padding: 12px;
-   }
-   .inf_img{
+.image-card-info {
     display: flex;
     align-items: center;
     gap: 10px;
     height: 30px;
-   }
-   .inf_time{
-    display: flex;
-    align-items: center;
-    gap:10px;
-    height: 30px;
-   }
+}
 </style>
