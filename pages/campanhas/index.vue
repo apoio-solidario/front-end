03e7476423f7ee935dce4716925fc1b780157da4
Lucas Josino <contact@lucasjosino.com>
2024-11-23 18:04:46 -0300

    feat: adiciona informação de SEO e dados dinâmicos para página de campanhas

--- pages/campanhas/index.vue
+++ pages/campanhas/index.vue
@@ -1,12 +1,21 @@
 <script lang="ts" setup>
+import { useLazyFetch } from '#app';
 import type { Campaign } from '~/shared/types/campaign';
 
-const campanhas: Campaign[] = []
+const { data: campaigns, status } = useLazyFetch<Campaign[]>('/api/campaigns')
+
+useSeoMeta({
+  title: 'Campanhas | Apoio Solidário',
+  ogTitle: 'Campanhas | Apoio Solidário',
+  description: 'Descubra campanhas solidárias para apoiar.',
+  ogDescription: 'Descubra campanhas solidárias para apoiar.',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
   <CardList>
-    <CampaignCard v-for="(item, index) in campanhas" :item="item" :index="index" :key="item.id" />
+    <CampaignCard v-for="(item, index) in campaigns" :item="item" :index="index" :key="item.id" />
   </CardList>
 </template>
 
