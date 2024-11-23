648f806e79cb833b0523a0b076884603da857f56
Lucas Josino <contact@lucasjosino.com>
2024-11-23 18:07:56 -0300

    feat: adiciona informação de SEO nas páginas de eventos e ongs

--- pages/eventos/index.vue
+++ pages/eventos/index.vue
@@ -2,6 +2,14 @@
 import type { Event } from '~/shared/types/event';
 
 const eventos: Event[] = []
+
+useSeoMeta({
+  title: 'Eventos | Apoio Solidário',
+  ogTitle: 'Eventos | Apoio Solidário',
+  description: 'Explore eventos beneficentes e oportunidades para contribuir.',
+  ogDescription: 'Explore eventos beneficentes e oportunidades para contribuir.',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
--- pages/ongs/index.vue
+++ pages/ongs/index.vue
@@ -2,6 +2,14 @@
 import type { ONG } from '~/shared/types/ong';
 
 const ongs: ONG[] = []
+
+useSeoMeta({
+  title: 'ONGs | Apoio Solidário',
+  ogTitle: 'ONGs | Apoio Solidário',
+  description: 'Descubra organizações e saiba como apoiá-las.',
+  ogDescription: 'Descubra organizações e saiba como apoiá-las.',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
