76daab95aac2a17b4cc29e2de1b3b230e985d272
JonatasGomes <jonatasgomes1090@gmail.com>
2024-11-30 10:28:56 -0300

    feat: adiciona páginas de listagem

--- pages/eventos/index.vue
+++ pages/eventos/index.vue
@@ -5,9 +5,129 @@ const eventos: Event[] = []
 </script>
 
 <template>
-  <CardList>
+  <main>
+    <h1>Eventos</h1>
+    <img src="/public/images/Cat.png" alt="" class="cat_img">
+    <CardList>
     <EventCard v-for="(item, index) in eventos" :item="item" :index="index" :key="item.id" />
-  </CardList>
+    </CardList>
+<hr>
+    <div class="Info_eventos">
+      <div class="imagens_descricao">
+          <div class="descricao">
+            <h1>Descrição</h1>
+
+            <h2 class="evento_solidario">🌟 Evento Solidário - Unidos pelo Bem 🌟</h2>
+            <p>Participe do nosso evento especial e ajude a transformar vidas! Este é um momento de união, solidariedade e impacto positivo na comunidade.</p>
+            <h2>📅 Informações do Evento:</h2>
+
+            <ul>
+              <li>Data: 25 de novembro de 2024</li>
+              <li>Horário: 14h às 20h</li>
+              <li>Local: Centro Comunitário Esperança, Rua Lorem Ipsum, 123 - São Paulo, SP</li>
+            </ul>
+
+            <h2>🎉 O que esperar?</h2>
+
+            <ul>
+              <li>Palestras inspiradoras: Conheça histórias de superação e aprenda com especialistas em temas como:</li>
+              <li>Sustentabilidade</li>
+              <li>Inclusão social</li>
+              <li>Empreendedorismo solidário</li>
+            </ul>
+            <h2>🎯 Como Participar?</h2>
+
+            <ul>
+              <li>Faça sua inscrição clicando aqui</li>
+              <li>Traga 1kg de alimento não perecível como contribuição.</li>
+              <li>Convide seus amigos e familiares!</li>
+            </ul>
+
+            <h2>🤝 Apoio e Parceiros</h2>
+            <p>Agradecemos às organizações que tornaram este evento possível:</p>
+
+            <ul>
+              <li>Instituto Lorem Ipsum</li>
+              <li>Fundação Solidária</li>
+              <li>Voluntários Unidos</li>
+            </ul>
+
+            <h2>✨ Juntos, podemos fazer a diferença!</h2>
+            <p>Não perca essa oportunidade de contribuir para um futuro melhor. Venha fazer parte dessa transformação!
+              Para mais informações, entre em contato:</p>
+              <p>📧 contato@eventosolidario.com </p>
+                <p>📞 (11) 1234-5678</p>
+          </div>
+      </div>
+      <hr class="hr2">
+      <div class="info_tags">
+        <div class="sobre">
+            <h1 class="h1_sobre">Imformações</h1>
+            <p class="P_info">📅 Sun, Sep 10, 2023 - Tue, Sep 12, 2023</p>
+            <p class="P_info">🕙09:00 AM - 05:00 PM</p>
+            <p class="P_info">📍
+              R. Todos os Santos, 286-552 - Salesianos, Juazeiro do Norte, Ceará</p>
+            <p>In enim tempora aut molestiae exercitationem et omnis modi et illo nulla 33 vitae aspernatur et sunt nulla. Est dolores cumque in quos facere ut quae temporibus sit.</p>
+          </div>
+          
+        <h1>tags</h1>
+        <div class="tags">
+            <button>Evento</button>
+            <button>Ceará</button>
+            <button>social</button>
+            <button>Comunidade</button>
+            <button>Cultura</button>
+          </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style></style>
+<style>
+main{
+  width: 100%;
+  padding: 5px;
+  margin-top: 30px;
+}
+.P_info{
+  margin: 5px 0px 5px 0px;
+}
+.teste{
+  width: 100%;
+  background-color: aqua;
+}
+.cat_img{
+  width: 70%;
+}
+.descricao{
+  width: 90%;
+}
+hr{
+  margin-top: 30px;
+  margin-bottom: 40px;
+  width: 70%;
+  border: 1px solid rgba(0, 0, 0, 0.432);
+}
+.hr2{
+  width: 1px;
+  position: relative;
+  height: 500px;
+  right:50px;
+  top: 25px;
+  border: 1px solid rgba(0, 0, 0, 0.432);
+}
+.Info_eventos{
+  display: flex;
+}
+.tags{
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 100%;
+  margin-top: 20px;
+}
+.info_tags{
+  margin-top:50px;
+}
+
+</style>
--- pages/ongs/index.vue
+++ pages/ongs/index.vue
@@ -1,13 +1,156 @@
 <script lang="ts" setup>
-import type { ONG } from '~/shared/types/ong';
+import type { ONG } from "~/shared/types/ong";
 
-const ongs: ONG[] = []
+const ongs: ONG[] = [];
 </script>
 
 <template>
-  <CardList>
-    <ONGCard v-for="(item, index) in ongs" :item="item" :index="index" :key="item.id" />
-  </CardList>
+  <div class="completo">
+    <main>
+      <img src="/images/image 8.png" alt="" class="logo_principal" />
+    </main>
+
+    <div class="logo_social">
+      <div class="logo_compartilhar">
+        <img src="/public/apple-touch-icon.png" alt="" class="img_social" />
+        <h1>Apoio Solidário</h1>
+        <label>Anumais</label>
+      </div>
+      <div class="botoes">
+        <a href="#" class="btn_">salvar</a>
+        <a href="#" class="btn_">Compartilhar</a>
+      </div>
+    </div>
+
+    <div class="informaçoes">
+      <h1>Sobre</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+      <ul>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+      </ul>
+    </div>
+    <h2>imagens</h2>
+
+    <div class="cartao_img">
+      <img src="/images/image 9.png" alt="" class="img_cartao" />
+      <img src="/images/image 9.png" alt="" class="img_cartao" />
+      <img src="/images/image 9.png" alt="" class="img_cartao" />
+      <img src="/images/image 9.png" alt="" class="img_cartao" />
+    </div>
+    <h2>Campanhas</h2>
+
+    <div class="campanhas">
+      <img src="/images/image 9.png" alt="" class="img_cartao1" />
+      <img src="/images/image 9.png" alt="" class="img_cartao1" />
+    </div>
+
+    <h2>Eventos</h2>
+
+    <section class="eventos">
+      <div class="event">
+        <img src="/images/image 9.png" alt="" class="img_cartao1" />
+      </div>
+      <div class="event">
+        <img src="/images/image 9.png" alt="" class="img_cartao1" />
+      </div>
+    </section>
+
+    <CardList>
+      <ONGCard
+        v-for="(item, index) in ongs"
+        :item="item"
+        :index="index"
+        :key="item.id"
+      />
+    </CardList>
+  </div>
 </template>
 
-<style></style>
+<style>
+main {
+  padding: 0;
+  margin: 0;
+}
+.logo_principal {
+  width: 100%;
+}
+.logo_social {
+  margin: 15px 30px 0px 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px 0px 15px;
+  position: relative;
+  bottom: 75px;
+}
+.img_social {
+  width: 90px;
+  border: 7px solid rgb(255, 255, 255);
+  border-radius: 50%;
+}
+.btn_ {
+  padding: 5px 16px 5px 16px;
+  margin: 10px;
+  border: 1px solid rgba(14, 14, 14, 0.486);
+  border-radius: 6px;
+  color: rgba(0, 0, 0, 0.568);
+  position: relative;
+  right: 70px;
+}
+.informaçoes {
+  padding: 10px 15px 12px 15px;
+  margin: 15px 30px 0px 30px;
+  width: 65%;
+}
+.cartao_img {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px 15px 0px 15px;
+  margin: 15px 30px 0px 30px;
+  gap: 10px;
+  width: 70%;
+}
+.img_cartao {
+  width: 170px;
+  height: 150px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 15px;
+  padding: 6px;
+}
+.campanhas {
+  padding: 10px 15px 0px 15px;
+  margin: 15px 30px 0px 30px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.img_cartao1 {
+  width: 270px;
+  height: 150px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 15px;
+  padding: 6px;
+}
+.eventos {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin: 15px 30px 0px 30px;
+  padding: 10px 15px 0px 15px;
+}
+h2 {
+  margin: 15px 30px 0px 30px;
+  padding: 10px 15px 0px 15px;
+}
+</style>
