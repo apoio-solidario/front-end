5bf00c2d206ec292a2edbff50e4c147233617a69
Lucas Josino <contact@lucasjosino.com>
2024-12-11 12:02:12 -0300

    feat: adiciona página inicial para campanhas no painel

---
+++ pages/app/campanhas/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: 'authenticated',
+  layout: 'dashboard',
+});
+</script>
+
+<template>
+  <div class="dashboard-campaigns">
+    Campanhas
+  </div>
+</template>
+
+<style>
+.dashboard-campaigns {
+  height: 100%;
+  width: 100%;
+  padding: var(--spacing-md);
+}
+
+.rounded-table {
+  border-radius: 12px;
+  overflow: hidden;
+  border: 1px solid var(--p-datatable-header-cell-border-color);
+}
+</style>
